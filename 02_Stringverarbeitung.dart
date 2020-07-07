c7bcd9be2d45d9c7d5704934477a86ac89c98928
JanH00 <67894980+JanH00@users.noreply.github.com>
2020-07-07 12:38:35 +0200

    Create 02_Stringverarbeitung.dart

---
+++ 02_Stringverarbeitung.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+  String meinName = "Jan";
+  int lange = meinName.length;
+  String klein = meinName.toLowerCase();
+  print("Mein Name ist $lange Zeichen lang");
+  print(klein);
+  
+  double zahl = 2.5;
+  String zahl2 = zahl.toString();
+  print("Das ist ein String "+ zahl2);
+  
+  
+}
